a861ad97010511afd3b938c952fd8b0bf9938ad2
Guillermo Saldaña <guillermosasu99@gmail.com>
2025-02-01 18:03:16 +0100

    First steps

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  imports: [
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ]
+})
+export class AppModule { }
